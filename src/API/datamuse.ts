ac190707316ca84b1e1199cb94c8e8b25f11aa32
Tariq Axel <tariqaxel@gmail.com>
2021-11-14 13:03:56 +0100

    API limit to 1000 and changing colors

--- src/API/datamuse.ts
+++ src/API/datamuse.ts
@@ -2,7 +2,7 @@ import {receivedDataType} from '../App'
 
 function fetchWordData(word: string,topics: string,mode: string): Promise<Array<receivedDataType>>{
     return new Promise((resolve, reject) =>{
-        const url = `https://api.datamuse.com/words?${mode}=${word}&topics=${topics}`
+        const url = `https://api.datamuse.com/words?${mode}=${word}&topics=${topics}&max=1000`
         fetch(url)
             .then((data)=>{return data.json()})
             .then((convertedData) => {resolve(convertedData)})
